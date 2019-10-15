2ea3917c223784b04b842ad003876c945d79f88b
GrownUprince <jake.lee4006@gmail.com>
2019-10-16 01:18:11 +0900

    실제로 백단에 데이터를 보내기 위해서 새 포스트 추가 관련 saga 업데이트

--- front/redux/sagas/post.js
+++ front/redux/sagas/post.js
@@ -1,4 +1,4 @@
-import { all, delay, fork, put, takeLatest, call } from "redux-saga/effects";
+import { all, fork, put, takeLatest, call } from "redux-saga/effects";
 import axios from "axios";
 import Router from "next/router";
 import { ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE } from "../modules/post";
@@ -11,15 +11,16 @@ function addPostAPI(postData) {
 
 function* addPost(action) {
 	try {
-		// const result = yield call(addPostAPI, action.data);
-		// yield delay(1000);
+		const result = yield call(addPostAPI, action.data);
+
 		yield put({
-			type: ADD_POST_SUCCESS
-			// data: result.data,
-    });
-    alert("새 포스트를 추가하였습니다");
-    Router.push("/");
+			type: ADD_POST_SUCCESS,
+			data: result.data
+		});
+		alert("새 포스트를 추가하였습니다");
+		Router.push("/");
 	} catch (e) {
+		console.error(e);
 		yield put({
 			type: ADD_POST_FAILURE,
 			payload: e.message
