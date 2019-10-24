4e69fd29b7a349c0530f984ba5da528bed20da65
GrownUprince <jake.lee4006@gmail.com>
2019-10-25 00:05:10 +0900

    _document.js로 render해주어도 styeld component SSR 적용 문제없이 작동하도록 수정

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -1,62 +1,40 @@
 import React from "react";
 // import Helmet from "react-helmet";
 import PropTypes from "prop-types";
-import Document, { Main, NextScript } from "next/document";
+import Document, { Main, NextScript, Head } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
-	static async getInitialProps(context) {
-		//styled component를 SSR로 적용하기 위해서 getInitialProps 내에서 세팅이 필요.
-		//이렇게 해주기 전에는 새로고침 시 styled component의 css가 적용이 되지 않은채로 잠깐 보여지는 현상이 발생한다. SSR이 적용된 컴포넌트들이 css 스타일 없이 html 뼈대만 갖춘채 렌더링되기 때문.
+	static getInitialProps({ renderPage }) {
+		// Step 1: Create an instance of ServerStyleSheet
 		const sheet = new ServerStyleSheet();
-		const originalRenderPage = context.renderPage;
 
-		try {
-			//_document.js가 _app.js의 상위 컴포넌트인데 _app.js에서 사용되는 getInitialProps가 제대로 동작하려면 _document.js에서도 하위 컴포넌트인 App(_app.js)을 렌더링해주는 부분이 있어야 한다.
-			context.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-				});
+		// Step 2: Retrieve styles from components in the page
+		const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
 
-			const initialProps = await Document.getInitialProps(context);
+		// Step 3: Extract the styles as <style> tags
+		const styleTags = sheet.getStyleElement();
 
-			return {
-				...initialProps,
-				// helmet: Helmet.renderStatic(),
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				)
-			};
-		} finally {
-			sheet.seal();
-		}
-
-		// const page = context.renderPage(App => props => sheet.collectStyles(<App {...props} />));
-		// console.log(page);
-		// const styleTags = sheet.getStyleElement();
-		// return { ...page, helmet: Helmet.renderStatic(), styleTags };
+		// Step 4: Pass styleTags as a prop
+		return { ...page, styleTags };
 	}
 
-	// render() {
-	// 	//아래와 같이 html이나 body 태그 등에 직접 attributes 세팅해주고 하려면 next에서는 _document.js 파일을 생성해야 해야 한다.
-	// 	//근본적으로 위와 같이 해주는 이유는 검색엔진 봇에 검색될 수 있게 하기 위함. 결국 SEO
-	// 	//<Main />은 각 라우트에 해당하는 페이지가 렌더링되는 부분이며,
-	// 	//<NextScript />는 Next.js 관련한 자바스크립트 파일이다.
-	// 	return (
-	// 		<html>
-	// 			<head>
-				
-	// 			</head>
-	// 			<body>
-	// 				<Main />
-	// 				<NextScript />
-	// 			</body>
-	// 		</html>
-	// 	);
-	// }
+	render() {
+		return (
+			<html>
+				<Head>
+					{/* Step 5: Output the styles in the head  */}
+					<meta charSet="utf-8" />
+					<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+					{this.props.styleTags}
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</html>
+		);
+	}
 }
 
 MyDocument.propTypes = {
