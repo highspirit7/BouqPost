dc87ead9330cfeb2134244dd0280ca0f0ca298c4
GrownUprince <jake.lee4006@gmail.com>
2019-10-26 20:13:03 +0900

    카카오 로그인 실패 및 성공시 리디렉션되는 url 배포인 경우 아닌 경우로 나누어 수정

--- back/routes/oauth.js
+++ back/routes/oauth.js
@@ -3,15 +3,21 @@ const passport = require("passport");
 
 const router = express.Router();
 
+const prod = process.env.NODE_ENV === "production";
+
+const redirectUrlSuccess = prod ? "http://bouqpost.xyz" : "http://localhost:3000";
+
+const redirectUrlFailure = prod ? "http://bouqpost.xyz/login" : "http://localhost:3000/login";
+
 router.get("/kakao", passport.authenticate("kakao"));
 
 router.get(
 	"/kakao/callback",
 	passport.authenticate("kakao", {
-    failureRedirect: "http://localhost:3000/login"
+		failureRedirect: redirectUrlFailure
 	}),
 	(req, res) => {
-		res.redirect("http://localhost:3000");
+		res.redirect(redirectUrlSuccess);
 	}
 );
 
