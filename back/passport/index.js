08370000720da92b1e4683ebc8224c33e7a76c97
GrownUprince <jake.lee4006@gmail.com>
2019-10-27 21:50:42 +0900

    deserializeUser logger 추가

--- back/passport/index.js
+++ back/passport/index.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const db = require("../models");
 const kakao = require("./kakaoStrategy");
+const logger = require("../logger");
 
 module.exports = () => {
 	//로그인 성공시 딱 한번 실행되면서 사용자 식별자(아마 쿠키)를 세션 스토어(서버에 위치)에 저장(by egoing)
@@ -16,7 +17,7 @@ module.exports = () => {
 		//  passport: { user: '카카오 유저 아이디' } }
 		// 프론트(브라우저)에서는 쿠키 값만 전달
 		// 프론트에서 요청이 들어올 시, 쿠키가 전달하는 유저 아이디를 이용하여 어떤 유저에 해당하는지 서버가 알 수 있게 된다.
-	
+
 		//사용자를 식별할 수 있는 고유한 값을 전달해야 한다. 내 경우에는 User 테이블에 user_id가 그 고유 값인데, 책 따라서 user_id가 아닌 id를 전달해서 아래 deserializeUser 함수에서 DB에서 해당 유저를 찾을 수 없었던 문제로 삽질.
 		return done(null, user.user_id);
 	});
@@ -44,6 +45,8 @@ module.exports = () => {
 			// console.log("deserializeUser - " + user);
 			//return 사용 안해도 무방하지만 해당 코드 뒤에 더 실행되는 부분이 없다는 것을 확실히 하기 위해 return 웬만하면 사용 추천(by ZeroCho)
 			// 그러나 async 함수는 return은 사용 안해도 되는건지 확실하지 않다.(by ZeroCho)
+			logger.info("deserializeUser");
+			logger.info(user.dataValues);
 			return done(null, user.dataValues); // req.user에 저장된다
 		} catch (e) {
 			console.error(e);
